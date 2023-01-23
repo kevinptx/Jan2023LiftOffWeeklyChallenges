68aadb954c2cad676e97edb424ab2adb4150e64b
Kevin Peery <kevin.peery@gmail.com>
2023-01-22 21:34:13 -0600

    week4 complete

--- week4-challenge.js
+++ week4-challenge.js
@@ -1,21 +1,20 @@
-function LongestWord(sen) { 
+function LongestWord(sen) {
+  // code goes here
+  //The match() method retrieves the result of matching a string against a regular expression.
+  //g modifier: global. All matches (don't return on first match)
+  //i modifier: insensitive. Case insensitive match (ignores case of [a-zA-Z])
+  let array = sen.match(/[a-z]+/gi);
 
-    // code goes here
-    //The match() method retrieves the result of matching a string against a regular expression.
-  
-   let array = sen.match(/[a-z]+/gi)
-  
-    let sorted = array.sort(function(a, b){
-      return b.length - a.length;
-    });
-    return sorted[0];
-  }
-     
-  // keep this function call here 
-  console.log(LongestWord(readline()));
- 
- 
- /*
+  let sorted = array.sort(function (a, b) {
+    return b.length - a.length;
+  });
+  return sorted[0];
+}
+
+// keep this function call here
+console.log(LongestWord(readline()));
+
+/*
 Longest Word
 Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
 Examples
